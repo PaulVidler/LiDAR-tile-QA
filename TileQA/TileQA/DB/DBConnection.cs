3ca3c4f07b5c2a2777cc0edb1b96e295512df49b
Paul Vidler <45616638+PaulVidler@users.noreply.github.com>
2019-05-19 18:13:27 +1000

    Backup.
    Added db connection class

---
+++ TileQA/TileQA/DB/DBConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Dapper;
+using System.Data.SQLite;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectDatabase
+{
+    public class DBConnection
+    {
+        public static string DbFile
+        {
+            get { return Environment.CurrentDirectory + "\tileQADb.sqlite"; }
+        }
+
+        public static SQLiteConnection SimpleDbConnection()
+        {
+            return new SQLiteConnection("Data Source=" + DbFile);
+        }
+    }
+}
